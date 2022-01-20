b168e527e358c1171967bba56ecb6f288e67a469
paulnana <92203271+paulnana@users.noreply.github.com>
2022-01-20 14:13:44 -0800

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "paulnana886@gmail.com";
+$to = "pauldayakar6125@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
